fa09d913b9c468c50aac2599651d0d2c16e452e1
Juyeong <90313504+devJayve@users.noreply.github.com>
2024-04-09 21:50:58 +0900

    Title: 숫자로 이루어진 사각형, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240409/숫자로 이루어진 사각형/rectangle-with-a-number.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    
+    int N;
+    cin >> N;
+
+    int num = 0;
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++) {
+            cout << num % 10 << " ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
