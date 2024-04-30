a5a253deab2a3024a96b6ac2d2da8124fa723516
Juyeong <90313504+devJayve@users.noreply.github.com>
2024-04-30 21:51:54 +0900

    Title: 빙빙 돌며 숫자 사각형 채우기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240430/빙빙 돌며 숫자 사각형 채우기/snail-number-square.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+using namespace std;
+
+#define MAX_LEN 100
+
+int ans[MAX_LEN][MAX_LEN];
+
+int row, col;
+
+int dx[4] = {0, 1, 0, -1};
+int dy[4] = {1, 0, -1, 0};
+
+bool isRange(int x, int y) {
+    return x >= 0 && x < row && y >= 0 && y < col;
+}
+
+int main() {
+    cin >> row >> col;
+
+    int num = 1;
+
+    int x,y = 0;
+    ans[x][y] = 1;
+    int dir = 0;
+
+    for (int i = 2; i <= row * col; i++) {
+        int nx = x + dx[dir], ny = y + dy[dir];
+        if (!isRange(nx,ny) || ans[nx][ny] >= 1) {
+            dir = (dir + 1) % 4;
+        }
+        x = x + dx[dir], y = y + dy[dir];
+        ans[x][y] = i;   
+    }
+    
+
+    for (int i = 0; i < col; i++) {
+        for (int j =0; j < row; j++) {
+            cout << ans[i][j] << " ";
+        }
+        cout << endl;
+    }
+
+
+
+    return 0;
+}
