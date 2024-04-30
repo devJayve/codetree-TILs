7e357e17e7bf573725710b36eb37012cc8cb7653
Juyeong <90313504+devJayve@users.noreply.github.com>
2024-04-30 22:12:13 +0900

    Title: 빙빙 돌며 숫자 사각형 채우기, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 240430/빙빙 돌며 숫자 사각형 채우기/snail-number-square.cpp
+++ 240430/빙빙 돌며 숫자 사각형 채우기/snail-number-square.cpp
@@ -2,45 +2,40 @@
 using namespace std;
 
 #define MAX_LEN 100
+#define DIR_LEN 4
 
 int ans[MAX_LEN][MAX_LEN];
+int dx[DIR_LEN] = {0,1,0,-1};
+int dy[DIR_LEN] = {1,0,-1,0};
 
-int row, col;
-
-int dx[4] = {0, 1, 0, -1};
-int dy[4] = {1, 0, -1, 0};
+int n,m;
 
 bool isRange(int x, int y) {
-    return x >= 0 && x < row && y >= 0 && y < col;
+    return x >= 0 && x < n && y >= 0 && y < m;
 }
 
 int main() {
-    cin >> row >> col;
-
-    int num = 1;
+    cin >> n >> m;
 
     int x,y = 0;
-    ans[x][y] = 1;
     int dir = 0;
+    ans[x][y] = 1;
 
-    for (int i = 2; i <= row * col; i++) {
+    for (int i = 2; i <= n*m; i++) {
         int nx = x + dx[dir], ny = y + dy[dir];
-        if (!isRange(nx,ny) || ans[nx][ny] >= 1) {
+        if (!isRange(nx, ny) || ans[nx][ny] != 0) {
             dir = (dir + 1) % 4;
         }
         x = x + dx[dir], y = y + dy[dir];
-        ans[x][y] = i;   
+        ans[x][y] = i;
     }
-    
 
-    for (int i = 0; i < row; i++) {
-        for (int j =0; j < col; j++) {
+    for (int i = 0; i< n; i++) {
+        for (int j = 0; j < m; j++) {
             cout << ans[i][j] << " ";
         }
         cout << endl;
     }
-
-
 
     return 0;
 }
