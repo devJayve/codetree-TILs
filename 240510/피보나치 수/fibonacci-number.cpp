6fdd7c97af37836116fc5cda5b4fa9e76d316722
Juyeong <90313504+devJayve@users.noreply.github.com>
2024-05-10 19:32:28 +0900

    Title: 피보나치 수, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240510/피보나치 수/fibonacci-number.cpp
+++ 240510/피보나치 수/fibonacci-number.cpp
@@ -10,15 +10,13 @@ int main() {
     cin >> n;
 
     arr[0] = 1;
-    arr[1] == 1;
+    arr[1] = 1;
 
-    if (n >= 2) {
         for (int i = 2; i <= n; i++) {
         arr[i] = arr[i-1] + arr[i-2];
     }
-    }
 
-    cout << arr[n];
+    cout << arr[n-1];
 
     return 0;
 }
