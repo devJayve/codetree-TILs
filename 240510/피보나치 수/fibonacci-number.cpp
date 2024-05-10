955f6f35c30375f50bbfcd05f9286f234afcaca9
Juyeong <90313504+devJayve@users.noreply.github.com>
2024-05-10 19:36:31 +0900

    Title: 피보나치 수, Time: 1086ms, Memory: 0MB, Status: Time Limit Exceed - CodeTree

--- 240510/피보나치 수/fibonacci-number.cpp
+++ 240510/피보나치 수/fibonacci-number.cpp
@@ -5,18 +5,36 @@ using namespace std;
 
 int arr[MAX_LEN];
 
+//  tabulation
+// int main() {
+//     int n;
+//     cin >> n;
+
+//     arr[0] = 1;
+//     arr[1] = 1;
+
+//         for (int i = 2; i <= n; i++) {
+//         arr[i] = arr[i-1] + arr[i-2];
+//     }
+
+//     cout << arr[n-1];
+
+//     return 0;
+// }
+
+// recursion + memoization
+
+int Fibo(int n) {
+    if (n <= 2) {
+        return 1;
+    }
+    else {
+        return Fibo(n-1) + Fibo(n-2);
+    }
+
+}
 int main() {
     int n;
     cin >> n;
-
-    arr[0] = 1;
-    arr[1] = 1;
-
-        for (int i = 2; i <= n; i++) {
-        arr[i] = arr[i-1] + arr[i-2];
-    }
-
-    cout << arr[n-1];
-
-    return 0;
+    cout <<Fibo(n);
 }
