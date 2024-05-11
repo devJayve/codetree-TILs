d9d31770a167feb760ca652960fff2a2a12eef01
Juyeong <90313504+devJayve@users.noreply.github.com>
2024-05-11 15:42:58 +0900

    Title: 아름다운 수, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240511/아름다운 수/beautiful-number.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+vector<int> answer;
+
+int n;
+
+int cnt = 0;
+
+// 333
+// 122
+// 221
+// 111
+void print() {
+    int tmp = answer[0];
+    int tmp_count = 1;
+
+    for (int i = 0; i < answer.size() -1; i++) {
+        if (answer[i] == answer[i+1]) {
+            tmp_count++;
+            if (i == answer.size() - 2 && tmp_count % tmp != 0) {
+                return;
+            }
+        } else {
+            if (tmp_count % tmp != 0) {
+                return;
+            }
+            tmp = answer[i+1];
+            tmp_count = 1;
+            if (i == answer.size() - 2 && tmp_count % tmp != 0) {
+                return;
+            }
+        }
+    }
+
+    // for (int i = 0; i < answer.size(); i++) {
+    //     answer[i]
+    // }
+
+    cnt++;
+}
+
+void choose(int curr_num) {
+    if (curr_num == n+1) {
+        print();
+        return;
+    }
+    
+    for (int i = 1; i <= 4; i++) {
+        answer.push_back(i);
+        choose(curr_num+1);
+        answer.pop_back();
+    }
+}
+int main() {
+
+    cin >> n;
+
+    choose(1);
+
+    cout << cnt;
+    
+    return 0;
+}
