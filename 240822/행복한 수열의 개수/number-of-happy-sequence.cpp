fec3c3be686cc69bc627fc852601612fc9489293
Dowoon <90313504+devJayve@users.noreply.github.com>
2024-08-22 17:00:08 +0900

    Title: 행복한 수열의 개수, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240822/행복한 수열의 개수/number-of-happy-sequence.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+using namespace std;
+
+#define N 100
+int arr[N][N];
+int m,n;
+
+bool isHappyNumsFromCol(int x) {
+    int contiCount = 0;
+    int lastValue = -1;
+    
+    for (int i = 0; i < n; i++) {
+        if (arr[x][i] == lastValue) {
+            contiCount++;
+            if (contiCount >= m) return true;
+        }
+        else {
+            contiCount = 1;
+        }
+        lastValue = arr[x][i];
+    }
+    return false;
+}
+
+bool isHappyNumsFromRow(int y) {
+    int contiCount = 0;
+    int lastValue = -1;
+    
+    for (int i = 0; i < n; i++) {
+        if (arr[i][y] == lastValue) {
+            contiCount++;
+            if (contiCount >= m) return true;
+        }
+        else {
+            contiCount = 1;
+        }
+        lastValue = arr[i][y];
+    }
+    return false;
+}
+
+
+
+
+int main() {
+    int happyNumsCount = 0;
+    cin >> n >> m;
+
+    for (int i = 0; i < n; i++)
+        for (int j = 0; j < n; j++)
+            cin >> arr[i][j];
+    
+    for (int i = 0; i < n; i++) {
+        if (isHappyNumsFromRow(i)) happyNumsCount++;
+        if (isHappyNumsFromCol(i)) happyNumsCount++;
+    }
+    
+    cout << happyNumsCount;
+    
+    return 0;
+}
