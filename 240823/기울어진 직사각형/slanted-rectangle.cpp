4f8fe5a6684218070843a795da2ffe9b5a6ef596
Dowoon <90313504+devJayve@users.noreply.github.com>
2024-08-23 17:35:30 +0900

    Title: 기울어진 직사각형, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240823/기울어진 직사각형/slanted-rectangle.cpp
@@ -0,0 +1,82 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+#define MAX 20
+int arr[MAX][MAX];
+int n;
+
+int dx[4] = {-1,-1,1,1};
+int dy[4] = {1,-1,-1,1};
+
+bool isAvailableRange(int x, int y) {
+    return (x >= 0 && x < n && y >= 0 && y < n);
+}
+
+int getMoveSum(int x, int y) {
+    int maxSum = 0;
+    int sum = 0;
+    int tmp = 0;
+    
+    int curX = x;
+    int curY = y;
+    
+    for (int i = 1; i < n; i++) {
+        sum = 0;
+        tmp = 0;
+        if (!isAvailableRange(curX + dx[0] * i, curY + dy[0] * i)) continue;
+        for (int j = 0; j < i; j++) {
+            // 1번 방향 이동
+            curX += dx[0];
+            curY += dy[0];
+            sum += arr[curX][curY];
+        }
+        
+        tmp = sum;
+        
+        for (int j = 0; j < n-i; j++) {
+            if (!isAvailableRange(curX + dx[1], curY + dy[1])) continue;
+            // 2번 방향 이동
+            curX += dx[1];
+            curY += dy[1];
+            sum += arr[curX][curY];
+            // 각각 동일한 횟수로 3,4번 방향으로 이동 가능한지 확인
+            for (int l = 0; l < i; l++) {
+                if (!isAvailableRange(curX + dx[2], curY + dy[2])) continue;
+                curX += dx[2];
+                curY += dy[2];
+                sum += arr[curX][curY];
+                
+            }
+            for (int k = 0; k <= j; k++) {
+                if (!isAvailableRange(curX + dx[3], curY + dy[3])) continue;
+                curX += dx[3];
+                curY += dy[3];
+                sum += arr[curX][curY];
+            }
+            maxSum = max(sum, maxSum);
+            sum = tmp;
+        }
+    }
+    return maxSum;
+}
+
+int main() {
+    int maxMoveSum = 0;
+    cin >> n;
+    
+    for (int i = 0; i < n; i++)
+        for (int j =0; j <n; j++)
+            cin >> arr[i][j];
+    
+    for (int i = 0; i < n; i++){
+        for (int j =0; j <n; j++) {
+            int moveSum = getMoveSum(i, j);
+            maxMoveSum = max(moveSum, maxMoveSum);
+        }
+    }
+    
+    cout << maxMoveSum;
+    
+    return 0;
+}
