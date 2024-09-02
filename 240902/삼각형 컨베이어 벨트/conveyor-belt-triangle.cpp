e89775ce8fff441fac9dff473a1c7d9ae1a052f8
Dowoon <90313504+devJayve@users.noreply.github.com>
2024-09-02 14:21:22 +0900

    Title: 삼각형 컨베이어 벨트, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240902/삼각형 컨베이어 벨트/conveyor-belt-triangle.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+using namespace std;
+
+int n, t;
+int arr[600];
+int tmp[600];
+
+void printMatrix() {
+    for (int i = 0; i < n; i++)
+        cout << arr[i] << " ";
+    cout << endl;
+    for (int i = n; i < n*2; i++)
+        cout << arr[i] << " ";
+    cout << endl;
+    for (int i = n*2; i < n*3; i++)
+        cout << arr[i] << " ";
+    cout << endl;
+}
+
+void moveArr() {
+    for (int i = 0; i < t; i++)
+        tmp[i] = arr[n*3-t+i];
+    
+    for (int i = n*3-1; i >= t; i--)
+        arr[i] = arr[i-t];
+    
+    for (int i = 0; i < t; i++)
+        arr[i] = tmp[i];
+}
+
+int main() {
+    cin >> n >> t;
+    
+    for (int i = 0; i < n*3; i++)
+        cin >> arr[i];
+    
+    t %= 600;
+    
+    moveArr();
+    
+    printMatrix();
+    
+    return 0;
+}
