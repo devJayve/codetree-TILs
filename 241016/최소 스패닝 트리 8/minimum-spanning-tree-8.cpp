9f4b5963060be34d96353d524e2e29e4e5de22e0
Dowoon <90313504+devJayve@users.noreply.github.com>
2024-10-16 23:16:51 +0900

    Title: 최소 스패닝 트리 8, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 241016/최소 스패닝 트리 8/minimum-spanning-tree-8.cpp
+++ 241016/최소 스패닝 트리 8/minimum-spanning-tree-8.cpp
@@ -5,22 +5,29 @@
 #define MAX_N 500
 using namespace std;
 
+bool visited[MAX_N];
+
 void prim(vector<pair<int,int>> (&graph)[MAX_N],
           int (&dist)[MAX_N],
-          priority_queue<pair<int,int>> &pq,
           int N
           ) {
+    priority_queue<pair<int,int>> pq = priority_queue<pair<int,int>>();
     
     for (int i = 1; i <= N; i++) {
         dist[i] = (int)1e9;
     }
     
-    dist[pq.top().second] = 0;
+    pq.push(make_pair(-0, 1));
+    dist[1] = 0;
     
     while (!pq.empty()) {
         int min_dist, min_index;
         tie(min_dist, min_index) = pq.top();
         pq.pop();
+        
+        if (visited[min_index]) continue;
+        
+        visited[min_index] = true;
         
         for (int i = 0; i < graph[min_index].size(); i++) {
             int target_index, target_dist;
@@ -28,6 +35,7 @@ void prim(vector<pair<int,int>> (&graph)[MAX_N],
             
             if (target_dist < dist[target_index]) {
                 dist[target_index] = target_dist;
+                pq.push(make_pair(-target_dist, target_index));
             }
         }
     }
@@ -47,16 +55,12 @@ int main() {
         cin >> x >> y >> w;
         graph[x].push_back(make_pair(y, w));
         graph[y].push_back(make_pair(x, w));
-        
-        pq.push(make_pair(-w, x));
-        pq.push(make_pair(-w, y));
     }
     
-    prim(graph, dist, pq, n);
+    prim(graph, dist, n);
     
     int ans = 0;
     for (int i = 1; i <= n; i++) {
-
         ans += dist[i];
     }
     
