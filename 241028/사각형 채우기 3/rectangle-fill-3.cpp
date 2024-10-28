fd2f8bbb872efa6c120d83c8c27b7cc02f3f48c4
Dowoon <90313504+devJayve@users.noreply.github.com>
2024-10-28 21:06:51 +0900

    Title: 사각형 채우기 3, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 241028/사각형 채우기 3/rectangle-fill-3.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#define DIV 1000000007
+#define MAX_N 1000
+using namespace std;
+
+long long fillRect(int N) {
+    long long dp[MAX_N + 1];
+    dp[1] = 2;
+    dp[2] = 7;
+    dp[3] = 22;
+    
+    for (int i = 4; i <= N; i++) {
+        dp[i] = dp[i-1] + dp[i-2];
+        dp[i] %= DIV;
+    }
+    
+    return dp[N];
+}
+
+
+int main() {
+    int N;
+    cin >> N;
+    long long ans = fillRect(N);
+    cout << ans << '\n';
+    
+}
