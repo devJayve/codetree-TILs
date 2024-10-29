32f9662b49f1ce147578d66649abf0ef9f2cb2b9
Dowoon <90313504+devJayve@users.noreply.github.com>
2024-10-29 23:31:25 +0900

    Title: 두 방향 탈출 가능 여부 판별하기, Time: 1012ms, Memory: 0MB, Status: Time Limit Exceed - CodeTree

---
+++ 241029/두 방향 탈출 가능 여부 판별하기/determine-escapableness-with-2-ways.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#define MAX_N 100
+using namespace std;
+
+int N, M;
+bool no_snake[MAX_N][MAX_N];
+bool visited[MAX_N][MAX_N];
+bool isReachEnd;
+int dir_x[2] = {0, 1};
+int dir_y[2] = {1, 0};
+
+bool isCanGo(int x, int y) {
+    return (no_snake[x][y] && x < N && x >= 0 && y < M && y >= 0);
+}
+
+void dfs(int x, int y) {
+    if (x == N-1 && y == M-1) {
+        isReachEnd = true;
+        return;
+    }
+    for (int i = 0; i < 2; i++) {
+        int new_x = x + dir_x[i];
+        int new_y = y + dir_y[i];
+        if (isCanGo(new_x, new_y)) {
+            dfs(new_x, new_y);
+        }
+    }
+}
+
+int main() {
+    cin >> N >> M;
+    for (int i = 0; i < N; i++)
+        for (int j = 0; j < M; j++)
+            cin >> no_snake[i][j];
+    
+    visited[0][0] = true;
+    dfs(0, 0);
+    
+    cout << isReachEnd;
+    
+    return 0;
+}
