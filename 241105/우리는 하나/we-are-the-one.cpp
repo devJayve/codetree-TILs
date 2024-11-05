3c8a0f919ad72a1352d3753f942978fc48ce44a1
Dowoon <90313504+devJayve@users.noreply.github.com>
2024-11-05 23:04:29 +0900

    Title: 우리는 하나, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 241105/우리는 하나/we-are-the-one.cpp
+++ 241105/우리는 하나/we-are-the-one.cpp
@@ -8,6 +8,7 @@ using namespace std;
 
 int n, k, u, d;
 int grid[MAX_N][MAX_N];
+int max_cnt;
 int dir_x[4] = {1, 0, -1, 0};
 int dir_y[4] = {0, 1, 0, -1};
 
@@ -19,16 +20,18 @@ bool isCanGo(int x, int y, int dist) {
 int bfs(int x, int y, bool (&visited)[MAX_N][MAX_N]) {
     queue<pair<int,int>> q = queue<pair<int,int>>();
     int move_cnt = 0;
+
+    if (visited[x][y]) return 0;
     
     q.push(make_pair(x, y));
     visited[x][y] = true;
     move_cnt++;
-    
+
     while (!q.empty()) {
         int cur_x, cur_y;
         tie(cur_x, cur_y) = q.front();
         q.pop();
-        
+
         for (int i =0; i < 4; i++) {
             int new_x = cur_x + dir_x[i];
             int new_y = cur_y + dir_y[i];
@@ -40,52 +43,51 @@ int bfs(int x, int y, bool (&visited)[MAX_N][MAX_N]) {
             }
         }
     }
-    
+
     return move_cnt;
 }
 
-int countCnt(int x, int y, int limit) {
-    bool visited[MAX_N][MAX_N] = {false};
-    int cnt = 0;
-    
-    for (int i = x % n; i < n + x; i++) {
-        for (int j = y % n; j < n + y; j++) {
-            if (limit == 0) break;
-            if (visited[i][j]) continue;
-            cnt += bfs(i, j, visited);
-            limit--;
+vector<int> pos;
+
+void recursion(int cur_num) {
+    if (cur_num == k) {
+        bool visited[MAX_N][MAX_N] = {false};
+        int cnt = 0;
+        
+        for (auto num : pos) {
+            cnt += bfs(num / n, num % n, visited);
         }
+        max_cnt = max(max_cnt, cnt);
+        return;
     }
-    
-    return cnt;
+    for (int i = 0; i < n*n; i++) {
+        pos.push_back(i);
+        recursion(cur_num + 1);
+        pos.pop_back();
+    }
 }
 
 int main() {
     cin >> n >> k >> u >> d;
-    
+
     for (int i = 0; i < n; i++)
         for (int j = 0; j < n; j++)
             cin >> grid[i][j];
     
-    
-    int max_cnt = 0;
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n; j++) {
-            max_cnt = max(max_cnt, countCnt(i, j, k));
-        }
-    }
-    
-    cout << max_cnt;
-    
-   
-    
+    recursion(0);
     
 
-    
-    
-    
-    
-    
-    
+    cout << max_cnt;
+
+
+
+
+
+
+
+
+
+
+
     return 0;
 }
