7064aecf7cba0c4996748200aa70249666922757
Dowoon <90313504+devJayve@users.noreply.github.com>
2025-01-22 12:06:07 +0900

    Title: 거품 정렬 구현, Time: 197ms, Memory: 15MB, Status: Passed - CodeTree

--- 250122/거품 정렬 구현/implement-bubble-sort.js
+++ 250122/거품 정렬 구현/implement-bubble-sort.js
@@ -1,9 +1,8 @@
 const fs =require("fs");
 
-let input = fs.readFileSync(0).toString().split('\n');
+let input = fs.readFileSync(0).toString().trim().split('\n');
 let n = parseInt(input[0]);
 let arr = input[1].split(' ').map(Number);
-
 
 for (let i = 0; i < arr.length; i++) {
     for (let j = 0; j < arr.length -i; j++) {
