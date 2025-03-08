170d2213b8e7df96deb3c3174bf9753ca5e6fb6d
Dowoon <90313504+devJayve@users.noreply.github.com>
2025-03-08 18:52:33 +0900

    Title: hashmap 기본, Time: 534ms, Memory: 28MB, Status: Passed - CodeTree

--- 250308/hashmap 기본/hashmap-basic.js
+++ 250308/hashmap 기본/hashmap-basic.js
@@ -3,22 +3,24 @@ const input = fs.readFileSync(0).toString().trim().split('\n');
 
 const n = Number(input[0]);
 
-console.log(input[1]);
-// const commands = input[1].split(' ').map(String);
-
 const map = new Map();
 
-// for (let i = 0; i < n; i++) {
-//     switch(commands[i]) {
-//         case "add":
+for (let i = 1; i <= n; i++) {
+    const commands = input[i].split(' ').map(String);
 
-//             map.set('')
-//             break;
-//         case "remove":
-//             break;
-//         case "find":
-//             break;
-//         default:
-//             throw new Error('unsupported Command!', commands[i]);
-//     }
-// }
+    switch(commands[0]) {
+        case "add":
+            map.set(commands[1], commands[2]);
+            break;
+        case "remove":
+            map.delete(commands[1]);
+            break;
+        case "find":
+            const result = map.get(commands[1]);
+            if (result) console.log(result);
+            else console.log('None');
+            break;
+        default:
+            throw new Error('unsupported Command!', commands[0]);
+    }
+}
