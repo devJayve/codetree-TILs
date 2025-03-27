41df823e3ae12aa64e5855641064dac909f5238a
Dowoon <90313504+devJayve@users.noreply.github.com>
2025-03-27 18:33:56 +0900

    Title: 그래프 탐색, Time: 183ms, Memory: 17MB, Status: Runtime Error - Codetree

---
+++ 250327/그래프 탐색/graph-traversal.js
@@ -0,0 +1,33 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split('\n');
+
+const [n, m] = input[0].split(' ').map(Number);
+const edges = [];
+for (let i = 1; i <= m; i++) {
+    edges.push(input[i].split(' ').map(Number));
+}
+
+
+const graph = Array.from(Array(1000), () => Array());
+const visited = Array(1000).fill(false);
+
+edges.forEach(([x, y]) => {
+    graph[x].push(y);
+    graph[y].push(x);
+});
+
+let ans = 0;
+visited[1] = true;
+
+function dfs(vertex) {
+    graph[vertex].forEach((curV) => {
+        if (!visited[curV]) {
+            ans++;
+            visited[curV] = true;
+            dfs(curV);
+        }
+    });
+}
+
+dfs(1);
+console.log(ans);
